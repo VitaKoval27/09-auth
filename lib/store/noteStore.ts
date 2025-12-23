7a5fdcd7ccd6c8a4d2306d2b15e8e6efa0bafcfb
VitaKoval27 <talisman1980@gmail.com>
2025-12-23 09:02:18 +0000

    homework-initial

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,5 +1,5 @@
 import {create} from "zustand";
-import { newNoteProps } from "../api";
+import { newNoteProps } from "../api/clientApi";
 import { persist } from 'zustand/middleware'
 
 type NoteDraftStore={
